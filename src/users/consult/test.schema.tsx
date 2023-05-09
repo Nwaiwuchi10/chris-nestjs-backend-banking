5ca56ee82f1d08dc2699f27b66736239239a4ac8
Nwaiwuchi10 <djnchrys@gmail.com>
2023-05-09 04:45:20 -0700

    vercel

---
+++ src/users/consult/test.schema.tsx
@@ -0,0 +1,18 @@
+import { Document, Schema, model } from 'mongoose';
+
+export interface UserAccount extends Document {
+  phoneNumber: string;
+  password: string;
+  accountNumber: string;
+}
+
+const userAccountSchema = new Schema<UserAccount>({
+  phoneNumber: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  accountNumber: { type: String, required: true, unique: true },
+});
+
+export const UserAccountModel = model<UserAccount>(
+  'UserAccount',
+  userAccountSchema,
+);
