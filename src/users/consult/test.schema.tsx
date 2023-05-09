28f5fbdd8daa2dc10da7e9b6a97fe9633823d237
Nwaiwuchi10 <djnchrys@gmail.com>
2023-05-09 05:31:46 -0700

    love

--- src/users/consult/test.schema.tsx
+++ src/users/consult/test.schema.tsx
@@ -4,12 +4,14 @@ export interface UserAccount extends Document {
   phoneNumber: string;
   password: string;
   accountNumber: string;
+  pin: string;
 }
 
 const userAccountSchema = new Schema<UserAccount>({
   phoneNumber: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   accountNumber: { type: String, required: true, unique: true },
+  pin: { type: String, required: true, unique: true },
 });
 
 export const UserAccountModel = model<UserAccount>(
