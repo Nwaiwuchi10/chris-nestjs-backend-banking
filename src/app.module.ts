0ba4f6bba83dcfa75625ac70b157ec70218e6068
Nwaiwuchi10 <djnchrys@gmail.com>
2023-05-09 19:27:55 -0700

    ors

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { MailModule } from './mail/mail.module';
-import { CorsModule } from 'cors';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -23,7 +23,6 @@ import { CorsModule } from 'cors';
     TransactionModule,
 
     MailModule,
-    CorsModule,
   ],
 
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// import { cors } from 'cors';
-import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
 
   await app.listen(5000);
 }
